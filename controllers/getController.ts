fe2d5693ac95b487b3b72e7e84dcce0a2a604ec5
Richard Ngo-Lam <rngolam@gmail.com>
2022-07-25 20:54:19 -0700

    Add error checking for quote array length

--- controllers/getController.ts
+++ controllers/getController.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse, AxiosError } from "axios";
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import IEndpoint from "../utils/IEndpoint";
 import HTTPError from "../utils/error";
 
@@ -7,22 +7,24 @@ const getQuoteByQuery = (endpoint: IEndpoint) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const { q: query } = req.query;
     if (typeof query !== "string" || !query) {
-      return next(new HTTPError("Query parameter 'q' must be of type string."));
+      return next(new HTTPError("'q' query parameter must be a string type."));
     }
 
     try {
       const response: AxiosResponse = await axios.get(
         `${endpoint.url}?${endpoint.param}=${query}`
       );
-      const randomQuote =
-        response.data[Math.floor(Math.random() * response.data.length)];
+      let randomQuote = null;
+      if (response.data.length > 0) {
+        randomQuote = response.data[Math.floor(Math.random() * response.data.length)];
+      }
       res.json(randomQuote);
     } catch (e) {
-      const err = new HTTPError("GET request failed");
+      const err = new HTTPError("GET request error");
       if (e instanceof AxiosError && e.response) {
         err.message = e.response.data.error;
         if (e.response.status) {
-          err.status = e.response.status
+          err.status = e.response.status;
         }
       } else if (e instanceof Error) {
         err.message = e.message;
